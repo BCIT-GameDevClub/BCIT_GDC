3c819e9a7f9b594afe5ae752f4ff62cae34ed925
fargakrishnanta <f_krishnanta@live.com>
2017-08-16 14:27:05 -0700

    Moved main page to main folder

---
+++ script/home.js
@@ -0,0 +1,42 @@
+	//**Scroll to top with animation**
+$(document).ready(function () {
+	var amountScrolled = 300;
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > amountScrolled) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+		$('#back-to-top').tooltip('hide');
+	});
+	   //Scroll body to 0px when clicked
+	$('#back-to-top').click(function () {
+		$('#back-to-top').tooltip('hide');
+		$('html, body').animate({
+			scrollTop: 0
+		}, 700);
+		return false;
+	});
+	$('#back-to-top').tooltip('show');
+        
+    //**Scrolling animation**
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+    
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+    
+    //**Pop-over effect**
+    $('[data-toggle="popover"]').popover(); 
+});
+
+
+    
+    
+
+
--- script/resources.js
+++ script/resources.js
@@ -1,4 +1,19 @@
-function changePage(page, pageID) {
-    if(page != 0) document.getElementById("f-cover-page").style.display = "none";
+var currentPageID = "f-cover-page";
+//The original z-index of the current page
+var currentPageZIndex = 0;
+
+function changePage(pageID) {
+    document.getElementById("f-cover-page").style.display = "none";
+    if(currentPageID != "f-cover-page"){
+        document.getElementById(currentPageID).style.zIndex = currentPageZIndex;
+    }
+    
+    
+    currentPageZIndex = parseInt(getComputedStyle(document.getElementById(pageID)).zIndex);
+    
+    console.log(currentPageZIndex);
+    console.log(pageID);
     document.getElementById(pageID).style.zIndex = "0";
+    
+    currentPageID = pageID;
 }
